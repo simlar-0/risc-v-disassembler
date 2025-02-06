3f0abf732b7b9084cb66b56092bf089f3a912606
yuvve <37124628+yuvve@users.noreply.github.com>
2025-02-06 10:23:15 +0100

    4 represent instructions in disassembler (#5)
    * Create skeleton for instruction parsing
    
    * Change structure
    
    * Add all RV32I base integer instructions
    
    * Add tests for bit extractor

---
+++ src/instructions.rs
@@ -0,0 +1,247 @@
+use crate::registers::Register;
+use crate::conditions::Condition;
+
+pub type Instruction32 = u32;
+pub enum DecodedInstruction32 {
+    RType {
+        opcode: u8,
+        rd: u8,
+        funct3: u8,
+        rs1: u8,
+        rs2: u8,
+        funct7: u8,
+    },
+    IType {
+        opcode: u8,
+        rd: u8,
+        funct3: u8,
+        rs1: u8,
+        imm: u16,
+    },
+    SType {
+        opcode: u8,
+        imm: u16,
+        funct3: u8,
+        rs1: u8,
+        rs2: u8,
+    },
+    BType {
+        opcode: u8,
+        imm: u16,
+        funct3: u8,
+        rs1: u8,
+        rs2: u8,
+    },
+    UType {
+        opcode: u8,
+        rd: u8,
+        imm: u32,
+    },
+    JType {
+        opcode: u8,
+        rd: u8,
+        imm: u32
+    },
+}
+
+#[allow(non_camel_case_types)]
+pub enum ParsedInstruction32 {
+    add {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    sub {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    xor {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    or {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    and {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    sll {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    srl {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    sra {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    slt {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    sltu {
+        rd: Register,
+        rs1: Register,
+        rs2: Register,
+    },
+    addi {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    xori {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    ori {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    andi {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    slli {
+        rd: Register,
+        rs1: Register,
+        shamt: u8,
+    },
+    srli {
+        rd: Register,
+        rs1: Register,
+        shamt: u8,
+    },
+    srai {
+        rd: Register,
+        rs1: Register,
+        shamt: u8,
+    },
+    slti {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    sltiu {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    lb {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    lh {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    lw {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    lbu {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    lhu {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    sb {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+    },
+    sh {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+    },
+    sw {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+    },
+    beq {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+        condition: Condition,
+    },
+    bne {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+        condition: Condition,
+    },
+    blt {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+        condition: Condition,
+    },
+    bge {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+        condition: Condition,
+    },
+    bltu {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+        condition: Condition,
+    },
+    bgeu {
+        rs1: Register,
+        rs2: Register,
+        imm: i32,
+        condition: Condition,
+    },
+    jal {
+        rd: Register,
+        imm: i32,
+    },
+    jalr {
+        rd: Register,
+        rs1: Register,
+        imm: i32,
+    },
+    lui {
+        rd: Register,
+        imm: i32,
+    },
+    auipc {
+        rd: Register,
+        imm: i32,
+    },
+    ecall,
+    ebreak,
+}
+
+pub trait DecodeInstruction32 {
+    fn decode_instruction32(&self) -> Result<DecodedInstruction32, &'static str>;
+}
+
+pub trait ParseInstruction32 {
+    fn parse_instruction32(&self) -> Result<ParsedInstruction32, &'static str>;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+mod helpers;
+mod registers;
 mod conditions;
+mod instructions;
+mod decoder;
+mod parser;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
