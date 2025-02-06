d51bdbff18daf322481c830d40678e6b824b216d
Simon Larsson <99037566+simonlarsson517@users.noreply.github.com>
2025-02-06 18:32:39 +0100

    Add parser for load instructions

--- src/parser/itype.rs
+++ src/parser/itype.rs
@@ -2,10 +2,49 @@ use crate::instructions::ParsedInstruction32;
 use crate::registers::Register;
 use crate::helpers::extract_bits;
 
-pub(crate) fn parse_itype32(_opcode: &u8, rd: &u8, funct3: &u8, rs1: &u8, imm: &i16) -> Result<ParsedInstruction32, &'static str> {
+pub(crate) fn parse_itype32(opcode: &u8, rd: &u8, funct3: &u8, rs1: &u8, imm: &i16) -> Result<ParsedInstruction32, &'static str> {
+    match opcode {
+        0b0000011 => parse_itype32_load(funct3, rd, rs1, imm),
+        0b0010011 => parse_itype32_alu(funct3, rd, rs1, imm),
+        _ => Err("Invalid opcode"),
+    }
+}
+
+fn parse_itype32_load(funct3: &u8, rd: &u8, rs1: &u8, imm: &i16) -> Result<ParsedInstruction32, &'static str> {
+    match funct3 {
+        0b000 => Ok(ParsedInstruction32::lb {
+            rd: Register::try_from(*rd)?,
+            rs1: Register::try_from(*rs1)?,
+            imm: *imm,
+        }),
+        0b001 => Ok(ParsedInstruction32::lh {
+            rd: Register::try_from(*rd)?,
+            rs1: Register::try_from(*rs1)?,
+            imm: *imm,
+        }),
+        0b010 => Ok(ParsedInstruction32::lw {
+            rd: Register::try_from(*rd)?,
+            rs1: Register::try_from(*rs1)?,
+            imm: *imm,
+        }),
+        0b100 => Ok(ParsedInstruction32::lbu {
+            rd: Register::try_from(*rd)?,
+            rs1: Register::try_from(*rs1)?,
+            imm: *imm,
+        }),
+        0b101 => Ok(ParsedInstruction32::lhu {
+            rd: Register::try_from(*rd)?,
+            rs1: Register::try_from(*rs1)?,
+            imm: *imm,
+        }),
+        _ => Err("Invalid funct3"),
+    }
+}
+
+fn parse_itype32_alu(funct3: &u8, rd: &u8, rs1: &u8, imm: &i16) -> Result<ParsedInstruction32, &'static str> {
     let imm_upper_bits = extract_bits(*imm, 5, 11)?;
     let shamt = extract_bits(*imm, 0, 4)?;
-    
+
     match funct3 {
         0b000 => Ok(ParsedInstruction32::addi {
             rd: Register::try_from(*rd)?,
@@ -69,8 +108,65 @@ mod tests {
     use crate::registers::Register;
 
     #[test]
-    fn test_parse_itype32_addi() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b000, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_load_lb() {
+        let result = parse_itype32_load(&0b000, &0b00001, &0b00010, &0b000000000001).unwrap();
+        assert_eq!(result, ParsedInstruction32::lb {
+            rd: Register::try_from(0b00001).unwrap(),
+            rs1: Register::try_from(0b00010).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_itype32_load_lh() {
+        let result = parse_itype32_load(&0b001, &0b00001, &0b00010, &0b000000000001).unwrap();
+        assert_eq!(result, ParsedInstruction32::lh {
+            rd: Register::try_from(0b00001).unwrap(),
+            rs1: Register::try_from(0b00010).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_itype32_load_lw() {
+        let result = parse_itype32_load(&0b010, &0b00001, &0b00010, &0b000000000001).unwrap();
+        assert_eq!(result, ParsedInstruction32::lw {
+            rd: Register::try_from(0b00001).unwrap(),
+            rs1: Register::try_from(0b00010).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_itype32_load_lbu() {
+        let result = parse_itype32_load(&0b100, &0b00001, &0b00010, &0b000000000001).unwrap();
+        assert_eq!(result, ParsedInstruction32::lbu {
+            rd: Register::try_from(0b00001).unwrap(),
+            rs1: Register::try_from(0b00010).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_itype32_load_lhu() {
+        let result = parse_itype32_load(&0b101, &0b00001, &0b00010, &0b000000000001).unwrap();
+        assert_eq!(result, ParsedInstruction32::lhu {
+            rd: Register::try_from(0b00001).unwrap(),
+            rs1: Register::try_from(0b00010).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_itype32_load_invalid_funct3() {
+        let result = parse_itype32_load(&0b110, &0b00001, &0b00010, &0b000000000001);
+        assert!(result.is_err());
+        assert_eq!(result.err(), Some("Invalid funct3"));
+    }
+
+    #[test]
+    fn test_parse_itype32_alu_addi() {
+        let result = parse_itype32_alu(&0b000, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::addi {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -79,8 +175,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_slli() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b001, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_slli() {
+        let result = parse_itype32_alu(&0b001, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::slli {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -89,8 +185,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_slti() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b010, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_slti() {
+        let result = parse_itype32_alu(&0b010, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::slti {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -99,8 +195,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_sltiu() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b011, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_sltiu() {
+        let result = parse_itype32_alu(&0b011, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::sltiu {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -109,8 +205,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_xori() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b100, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_xori() {
+        let result = parse_itype32_alu(&0b100, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::xori {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -119,8 +215,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_srli() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b101, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_srli() {
+        let result = parse_itype32_alu(&0b101, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::srli {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -129,8 +225,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_srai() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b101, &0b00010, &0b010000000001).unwrap();
+    fn test_parse_itype32_alu_srai() {
+        let result = parse_itype32_alu(&0b101, &0b00001, &0b00010, &0b010000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::srai {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -139,8 +235,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_ori() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b110, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_ori() {
+        let result = parse_itype32_alu(&0b110, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::ori {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -149,8 +245,8 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_andi() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b111, &0b00010, &0b000000000001).unwrap();
+    fn test_parse_itype32_alu_andi() {
+        let result = parse_itype32_alu(&0b111, &0b00001, &0b00010, &0b000000000001).unwrap();
         assert_eq!(result, ParsedInstruction32::andi {
             rd: Register::try_from(0b00001).unwrap(),
             rs1: Register::try_from(0b00010).unwrap(),
@@ -159,15 +255,15 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_itype32_invalid_funct3() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b1000, &0b00010, &0b000000000001);
+    fn test_parse_itype32_alu_invalid_funct3() {
+        let result = parse_itype32_alu(&0b1000, &0b00001, &0b00010, &0b000000000001);
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Invalid funct3"));
     }
 
     #[test]
-    fn test_parse_itype32_invalid_imm() {
-        let result = parse_itype32(&0b0010011, &0b00001, &0b101, &0b00010, &0b100000000001);
+    fn test_parse_itype32_alu_invalid_imm() {
+        let result = parse_itype32_alu(&0b101, &0b00001, &0b00010, &0b100000000001);
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Invalid imm"));
     }
