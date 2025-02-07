b1fc7bf37c0390d62fda4720ad734833546c2cc3
Simon Larsson <99037566+simonlarsson517@users.noreply.github.com>
2025-02-07 13:16:17 +0100

    Add parser for stype

--- src/parser/stype.rs
+++ src/parser/stype.rs
@@ -1,7 +1,66 @@
 use crate::instructions::ParsedInstruction32;
 use crate::registers::Register;
-use crate::helpers::extract_bits;
 
-pub(crate) fn parse_stype32(opcode: &u8, imm: &i16, funct3: &u8, rs1: &u8, rs2: &u8) -> Result<ParsedInstruction32, &'static str> {
-    todo!()
+pub(crate) fn parse_stype32(_opcode: &u8, imm: &i16, funct3: &u8, rs1: &u8, rs2: &u8) -> Result<ParsedInstruction32, &'static str> {
+    match funct3 {
+        0b000 => Ok(ParsedInstruction32::sb {
+            rs1: Register::try_from(*rs1)?,
+            rs2: Register::try_from(*rs2)?,
+            imm: *imm,
+        }),
+        0b001 => Ok(ParsedInstruction32::sh {
+            rs1: Register::try_from(*rs1)?,
+            rs2: Register::try_from(*rs2)?,
+            imm: *imm,
+        }),
+        0b010 => Ok(ParsedInstruction32::sw {
+            rs1: Register::try_from(*rs1)?,
+            rs2: Register::try_from(*rs2)?,
+            imm: *imm,
+        }),
+        _ => Err("Invalid funct3"),
+    }   
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::registers::Register;
+
+    #[test]
+    fn test_parse_stype32_sb() {
+        let result = parse_stype32(&0b0100011, &0b000000000001, &0b000, &0b00010, &0b00011).unwrap();
+        assert_eq!(result, ParsedInstruction32::sb {
+            rs1: Register::try_from(0b00010).unwrap(),
+            rs2: Register::try_from(0b00011).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_stype32_sh() {
+        let result = parse_stype32(&0b0100011, &0b000000000001, &0b001, &0b00010, &0b00011).unwrap();
+        assert_eq!(result, ParsedInstruction32::sh {
+            rs1: Register::try_from(0b00010).unwrap(),
+            rs2: Register::try_from(0b00011).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_stype32_sw() {
+        let result = parse_stype32(&0b0100011, &0b000000000001, &0b010, &0b00010, &0b00011).unwrap();
+        assert_eq!(result, ParsedInstruction32::sw {
+            rs1: Register::try_from(0b00010).unwrap(),
+            rs2: Register::try_from(0b00011).unwrap(),
+            imm: 0b000000000001,
+        });
+    }
+
+    #[test]
+    fn test_parse_stype32_invalid_funct3() {
+        let result = parse_stype32(&0b0100011, &0b000000000001, &0b011, &0b00010, &0b00011);
+        assert!(result.is_err());
+        assert_eq!(result.err(), Some("Invalid funct3"));
+    }
 }
