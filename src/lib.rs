8f826b7e101f7a52876873a6e012f97a30fe49be
yuvve <37124628+yuvve@users.noreply.github.com>
2025-05-30 11:03:13 +0200

    Add doctest and update readme

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,6 @@
 //!     ParsedInstruction32,
 //!     parsed_instructions::*
 //! };
-//! use risc_v_disassembler::Register;
 //!
 //! let bytes = [0x93, 0x00, 0x51, 0x00];
 //! let is_big_endian = false;
@@ -38,6 +37,25 @@
 //! }));
 //! ```
 //!
+//! Or using ABI register names:
+//! ```
+//! use risc_v_disassembler::{
+//!     parse,
+//!     ParsedInstruction32,
+//!     parsed_instructions::*
+//! };
+//!
+//! let bytes = [0x93, 0x00, 0x41, 0x00];
+//! let is_big_endian = false;
+//! let use_abi_register_names = true;
+//! let parsed_instruction = parse(&bytes, is_big_endian, use_abi_register_names).unwrap();
+
+//! assert_eq!(parsed_instruction, ParsedInstruction32::addi (addi {
+//!    rd: "ra".to_string(),
+//!    rs1: "sp".to_string(),
+//!    imm: 4
+//! }));
+//! ``` `
 
 mod decoder;
 mod instructions;
