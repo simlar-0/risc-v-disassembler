a7892e8b30f57cefa9313e3a709840323ef170fb
yuvve <37124628+yuvve@users.noreply.github.com>
2025-05-30 09:08:26 +0200

    Add register trait

--- src/registers.rs
+++ src/registers.rs
@@ -1,10 +1,37 @@
 use crate::DisassemblerError;
 use std::fmt;
 
+pub trait Register {
+    fn as_u8(&self) -> u8;
+    fn from_u8(value: u8) -> Result<Self, DisassemblerError>
+    where
+        Self: Sized;
+}
+
+impl Register for NumberedRegister {
+    fn as_u8(&self) -> u8 {
+        *self as u8
+    }
+
+    fn from_u8(value: u8) -> Result<Self, DisassemblerError> {
+        NumberedRegister::try_from(value)
+    }
+}
+
+impl Register for ABIRegister {
+    fn as_u8(&self) -> u8 {
+        *self as u8
+    }
+
+    fn from_u8(value: u8) -> Result<Self, DisassemblerError> {
+        ABIRegister::try_from(value)
+    }
+}
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 #[repr(u8)]
 #[allow(non_camel_case_types)]
-pub enum Register {
+pub enum NumberedRegister {
     x0 = 0,
     x1 = 1,
     x2 = 2,
@@ -77,43 +104,43 @@ pub enum ABIRegister {
     t6 = 31,
 }
 
-impl TryFrom<u8> for Register {
+impl TryFrom<u8> for NumberedRegister {
     type Error = DisassemblerError;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            0 => Ok(Register::x0),
-            1 => Ok(Register::x1),
-            2 => Ok(Register::x2),
-            3 => Ok(Register::x3),
-            4 => Ok(Register::x4),
-            5 => Ok(Register::x5),
-            6 => Ok(Register::x6),
-            7 => Ok(Register::x7),
-            8 => Ok(Register::x8),
-            9 => Ok(Register::x9),
-            10 => Ok(Register::x10),
-            11 => Ok(Register::x11),
-            12 => Ok(Register::x12),
-            13 => Ok(Register::x13),
-            14 => Ok(Register::x14),
-            15 => Ok(Register::x15),
-            16 => Ok(Register::x16),
-            17 => Ok(Register::x17),
-            18 => Ok(Register::x18),
-            19 => Ok(Register::x19),
-            20 => Ok(Register::x20),
-            21 => Ok(Register::x21),
-            22 => Ok(Register::x22),
-            23 => Ok(Register::x23),
-            24 => Ok(Register::x24),
-            25 => Ok(Register::x25),
-            26 => Ok(Register::x26),
-            27 => Ok(Register::x27),
-            28 => Ok(Register::x28),
-            29 => Ok(Register::x29),
-            30 => Ok(Register::x30),
-            31 => Ok(Register::x31),
+            0 => Ok(NumberedRegister::x0),
+            1 => Ok(NumberedRegister::x1),
+            2 => Ok(NumberedRegister::x2),
+            3 => Ok(NumberedRegister::x3),
+            4 => Ok(NumberedRegister::x4),
+            5 => Ok(NumberedRegister::x5),
+            6 => Ok(NumberedRegister::x6),
+            7 => Ok(NumberedRegister::x7),
+            8 => Ok(NumberedRegister::x8),
+            9 => Ok(NumberedRegister::x9),
+            10 => Ok(NumberedRegister::x10),
+            11 => Ok(NumberedRegister::x11),
+            12 => Ok(NumberedRegister::x12),
+            13 => Ok(NumberedRegister::x13),
+            14 => Ok(NumberedRegister::x14),
+            15 => Ok(NumberedRegister::x15),
+            16 => Ok(NumberedRegister::x16),
+            17 => Ok(NumberedRegister::x17),
+            18 => Ok(NumberedRegister::x18),
+            19 => Ok(NumberedRegister::x19),
+            20 => Ok(NumberedRegister::x20),
+            21 => Ok(NumberedRegister::x21),
+            22 => Ok(NumberedRegister::x22),
+            23 => Ok(NumberedRegister::x23),
+            24 => Ok(NumberedRegister::x24),
+            25 => Ok(NumberedRegister::x25),
+            26 => Ok(NumberedRegister::x26),
+            27 => Ok(NumberedRegister::x27),
+            28 => Ok(NumberedRegister::x28),
+            29 => Ok(NumberedRegister::x29),
+            30 => Ok(NumberedRegister::x30),
+            31 => Ok(NumberedRegister::x31),
             _ => Err(DisassemblerError::InvalidRegister(value)),
         }
     }
@@ -161,7 +188,7 @@ impl TryFrom<u8> for ABIRegister {
     }
 }
 
-impl fmt::Display for Register {
+impl fmt::Display for NumberedRegister {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "x{}", *self as u8)
     }
@@ -212,38 +239,38 @@ mod tests {
 
     #[test]
     fn test_try_from_register() {
-        assert_eq!(0.try_into(), Ok(Register::x0));
-        assert_eq!(1.try_into(), Ok(Register::x1));
-        assert_eq!(2.try_into(), Ok(Register::x2));
-        assert_eq!(3.try_into(), Ok(Register::x3));
-        assert_eq!(4.try_into(), Ok(Register::x4));
-        assert_eq!(5.try_into(), Ok(Register::x5));
-        assert_eq!(6.try_into(), Ok(Register::x6));
-        assert_eq!(7.try_into(), Ok(Register::x7));
-        assert_eq!(8.try_into(), Ok(Register::x8));
-        assert_eq!(9.try_into(), Ok(Register::x9));
-        assert_eq!(10.try_into(), Ok(Register::x10));
-        assert_eq!(11.try_into(), Ok(Register::x11));
-        assert_eq!(12.try_into(), Ok(Register::x12));
-        assert_eq!(13.try_into(), Ok(Register::x13));
-        assert_eq!(14.try_into(), Ok(Register::x14));
-        assert_eq!(15.try_into(), Ok(Register::x15));
-        assert_eq!(16.try_into(), Ok(Register::x16));
-        assert_eq!(17.try_into(), Ok(Register::x17));
-        assert_eq!(18.try_into(), Ok(Register::x18));
-        assert_eq!(19.try_into(), Ok(Register::x19));
-        assert_eq!(20.try_into(), Ok(Register::x20));
-        assert_eq!(21.try_into(), Ok(Register::x21));
-        assert_eq!(22.try_into(), Ok(Register::x22));
-        assert_eq!(23.try_into(), Ok(Register::x23));
-        assert_eq!(24.try_into(), Ok(Register::x24));
-        assert_eq!(25.try_into(), Ok(Register::x25));
-        assert_eq!(26.try_into(), Ok(Register::x26));
-        assert_eq!(27.try_into(), Ok(Register::x27));
-        assert_eq!(28.try_into(), Ok(Register::x28));
-        assert_eq!(29.try_into(), Ok(Register::x29));
-        assert_eq!(30.try_into(), Ok(Register::x30));
-        assert_eq!(31.try_into(), Ok(Register::x31));
+        assert_eq!(0.try_into(), Ok(NumberedRegister::x0));
+        assert_eq!(1.try_into(), Ok(NumberedRegister::x1));
+        assert_eq!(2.try_into(), Ok(NumberedRegister::x2));
+        assert_eq!(3.try_into(), Ok(NumberedRegister::x3));
+        assert_eq!(4.try_into(), Ok(NumberedRegister::x4));
+        assert_eq!(5.try_into(), Ok(NumberedRegister::x5));
+        assert_eq!(6.try_into(), Ok(NumberedRegister::x6));
+        assert_eq!(7.try_into(), Ok(NumberedRegister::x7));
+        assert_eq!(8.try_into(), Ok(NumberedRegister::x8));
+        assert_eq!(9.try_into(), Ok(NumberedRegister::x9));
+        assert_eq!(10.try_into(), Ok(NumberedRegister::x10));
+        assert_eq!(11.try_into(), Ok(NumberedRegister::x11));
+        assert_eq!(12.try_into(), Ok(NumberedRegister::x12));
+        assert_eq!(13.try_into(), Ok(NumberedRegister::x13));
+        assert_eq!(14.try_into(), Ok(NumberedRegister::x14));
+        assert_eq!(15.try_into(), Ok(NumberedRegister::x15));
+        assert_eq!(16.try_into(), Ok(NumberedRegister::x16));
+        assert_eq!(17.try_into(), Ok(NumberedRegister::x17));
+        assert_eq!(18.try_into(), Ok(NumberedRegister::x18));
+        assert_eq!(19.try_into(), Ok(NumberedRegister::x19));
+        assert_eq!(20.try_into(), Ok(NumberedRegister::x20));
+        assert_eq!(21.try_into(), Ok(NumberedRegister::x21));
+        assert_eq!(22.try_into(), Ok(NumberedRegister::x22));
+        assert_eq!(23.try_into(), Ok(NumberedRegister::x23));
+        assert_eq!(24.try_into(), Ok(NumberedRegister::x24));
+        assert_eq!(25.try_into(), Ok(NumberedRegister::x25));
+        assert_eq!(26.try_into(), Ok(NumberedRegister::x26));
+        assert_eq!(27.try_into(), Ok(NumberedRegister::x27));
+        assert_eq!(28.try_into(), Ok(NumberedRegister::x28));
+        assert_eq!(29.try_into(), Ok(NumberedRegister::x29));
+        assert_eq!(30.try_into(), Ok(NumberedRegister::x30));
+        assert_eq!(31.try_into(), Ok(NumberedRegister::x31));
     }
 
     #[test]
@@ -282,4 +309,3 @@ mod tests {
         assert_eq!(31.try_into(), Ok(ABIRegister::t6));
     }
 }
-
